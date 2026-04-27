dfb23efa15260d26317127b4a0eefb1ea0a58c12
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-26 23:26:57 -0300

    Passando parametros para os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import './App.css'
 
 // no react componentes são FUNÇÕES
 
+// props é um OBJETO
+function TituloFormulario (props) {
+  return (
+    <h2>
+      {props.children}
+    </h2>
+  )
+}
+
 function FormularioDeEvento () {
   return (
     <form className='form-evento'>
-      <h2>Preencha para criar um evento:</h2>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
       <fieldset>
         <label htmlFor="nome">
           Qual o nome do evento?
         </label>
-        <input type="text" name="" id="nome" />
+        <input type='text' id='nome' placeholder='Summer dev hits' />
       </fieldset>
     </form>
   )
